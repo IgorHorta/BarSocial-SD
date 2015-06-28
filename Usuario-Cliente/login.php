64cba7351f192c6abae6458590f5e042a14385e8
igorlacerdino <igor3m@sistemas.ufla.br>
2015-06-28 14:53:02 -0300

    login do usuario

---
+++ Usuario-Cliente/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Flat UI Free</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="css/vendor/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="../../dist/js/vendor/html5shiv.js"></script>
+      <script src="../../dist/js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+    <body class="login-screen">
+
+      <h1 style="color:#fff;" class="text-center">Entrar no Bar Social</h1>
+          
+
+          <div class="login-form">
+            <div class="form-group">
+              <input type="text" class="form-control login-field" value="" placeholder="Informe o nome de usuário" id="nome_usuario" />
+              <label class="login-field-icon fui-user" for="nome_usuario"></label>
+            </div>
+
+            <div class="form-group">
+              <input type="password" class="form-control login-field" value="" placeholder="Senha" id="senha" />
+              <label class="login-field-icon fui-lock" for="senha"></label>
+            </div>
+
+            <a class="btn btn-primary btn-lg btn-block" href="#">Entrar</a>
+            <a class="login-link" href="cadastro.php">Cadastre-se</a>
+          </div>
+
+     
+        </body>
+</html>
