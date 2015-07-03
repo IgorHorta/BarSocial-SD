aeed04d37eae9e42158b053ccd9eba358c36d7af
IGOR LACERDINO <igor3m@gmail.com>
2015-07-03 18:07:13 -0300

    mudança na tela de cadastro display none

---
+++ Usuario-Cliente/index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Flat UI Free</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="css/vendor/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+    
+    
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="../../dist/js/vendor/html5shiv.js"></script>
+      <script src="../../dist/js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <style>
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <div class="container">
+     
+        
+<div id="cadastro">
+      <div class="row">
+          <div class="col-md-6">
+              
+              <img src="img/logo.jpg" class="center-block">
+          </div>
+        <div class="col-md-6">
+          <h4 class="text-center">Cadastro de usuário</h4>
+          <form role="form">
+          
+            <div class="form-group">
+              <label for="exampleInputEmail1">Nome de usuário:</label>
+              <input type="text" class="form-control" id="exampleInputText1" name="user.name" placeholder="Informe um nome de usuário">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Senha:</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" name="user.password" placeholder="Senha">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Repita a senha:</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" name="user.password" placeholder="Repetir Senha">
+            </div>
+            <button type="submit" class="btn btn-warning btn-lg">Cadastrar</button>
+          
+		  </form>
+        </div><!-- /.col-md-12 -->
+      </div><!-- /.row -->
+</div> <!-- fim da tela de cadastro -->
+        
+        
+<div id="cadastro_pedido" style="display:none;">
+         <div class="row">
+          <div class="col-md-4">
+              <h3 class="text-center">Cadastrar novo pedido</h3>
+               <img src="img/logo.jpg" class="center-block" style="padding-top:20px;">
+          </div>
+        <div class="col-md-8">
+         <form action="#" class="form">
+             <h4>Marque o item que deseja pedir:</h4>
+            <div class="form-group form-inline">
+             
+              <label class="radio" for="item1">
+                <input type="radio" name="optionsRadios" data-toggle="radio" value="" id="item1" required checked>
+                Bolo<img src="img/bolo.jpg" class="img-responsive">
+              </label>
+              <label class="radio" for="item2">
+                <input type="radio" name="optionsRadios" data-toggle="radio" value="" id="item2" required>
+                 Hamburger<img src="img/burguer.jpg" class="img-responsive">
+              </label>
+              <label class="radio" for="item3">
+                <input type="radio" name="optionsRadios" data-toggle="radio" value="" id="item3" required>
+                 Cerveja<img src="img/cerveja.jpg" class="img-responsive">
+              </label>
+                <label class="radio" for="item4">
+                <input type="radio" name="optionsRadios" data-toggle="radio" value="" id="item4" required>
+                 Sushi<img src="img/sushi.jpg" class="img-responsive">
+              </label>
+                 <label class="radio" for="item5">
+                <input type="radio" name="optionsRadios" data-toggle="radio" value="" id="item5" required>
+                 Hot-dog<img src="img/doge.jpg" class="img-responsive">
+              </label>
+            </div>
+             <div class="row">
+            <div class="form-group">
+            <div class="form-group form-group-hg">
+            <label class="col-sm-4 control-label" for="formGroupInputHuge">Selecione a quantidade:</label>
+                <div class="col-sm-8">
+              <select data-toggle="select" class="form-control select select-primary">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10</option>
+              </select>
+                </div>
+            </div>
+             </div>
+                 </div>
+           
+             <h4>Observações</h4>
+          
+            <div class="form-group">
+              <textarea class="form-control" rows="3"></textarea>
+            </div>
+              <button type="submit" class="btn btn-primary btn-lg" id="envia_pedido">Enviar Pedido</button>
+          </form>
+        </div>
+      </div><!-- /.row -->
+</div> <!-- fim cadastro pedido -->
+        
+<div id="status" style="display:none;">
+        
+        <div class="row">
+          <div class="col-md-4">
+              <h3 class="text-center">Olá Igor!</h3>
+               <img src="img/logo.jpg" class="center-block" style="padding-top:20px;">
+          </div>
+        <div class="col-md-8">
+         <form action="#" class="form">
+             <h4>Status do seu pedido:</h4>
+           <div class="jumbotron">
+        <h1>Cozinhando</h1>
+        <p class="lead">Estamos preparando tudo para ficar perfeito para você</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button" id="sair">Sair</a></p>
+      </div>
+           
+            
+        </div>
+      </div><!-- /.row -->
+</div> <!-- fim da div status -->
+        
+        
+    </div><!-- /.container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/vendor/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/flat-ui.min.js"></script>
+    <script src="js/cadastro.js"> </script>
+    <script src="js/application.js"></script>
+
+  </body>
+</html>
